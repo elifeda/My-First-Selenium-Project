756dfdf45114886ceacb6a0debf2bd19a3ad94f1
Elif Eda Güneş <36040744+elifeda@users.noreply.github.com>
2019-07-04 21:09:26 +0300

    updated

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,33 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        IWebDriver driver;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            driver = new ChromeDriver();
+            driver.Url = "https://google.com";
+            driver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div/div[1]/div/div[1]/input")).SendKeys("Elif Eda Güneş");
+            driver.FindElement(By.XPath("//*[@id='tsf']/div[2]/div/div[3]/center/input[1]")).Click();
+
+        }
+    }
+}
